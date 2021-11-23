6f26cc2fd6e1b7d07728c909e50eef10c7fae67c
catherinerlopezv <catherinerlopezv@gmail.com>
2021-11-23 16:19:18 -0600

    Add files via upload

---
+++ Utilities/GlobalData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Projecto.Models;
+
+namespace Projecto
+{
+    public class GlobalData
+    {
+        public UserData ActualUser { get; set; }
+        public UserData Receptor { get; set; }
+        public string para { get; set; }
+        public string ArchivoEntrada { get; set; }
+        public string ArchivoSalida { get; set; }
+    }
+}
