57462204f3ee6cf7531ed846fb0ecccd340a93a7
Zer0 __ <gabrielyomolina@outlook.es>
2021-11-25 18:06:01 -0600

    Modelos

--- Projecto/Models/MensajesViewModel - Copy.cs
+++ Projecto/Models/MensajesViewModel - Copy.cs
@@ -7,15 +7,15 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace Projecto.Models
 {
-    public class MensajesViewModel
+    public class MensajesViewModelG
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; } //Id que maneja mongodb
         public string Emisor { get; set; } //Emisor del mensaje
         public List<string> Receptor { get; set; } //Receptor del mensaje
-        public string Cuerpo { get; set; } //El mensaje en sí 
         public string Grupo { get; set; }
+        public string Cuerpo { get; set; } //El mensaje en sí 
         public string Visible { get; set; } //identificador para borrar o no el mensaje
         public DateTime Date { get; set; }
         public bool Archivo { get; set; }
